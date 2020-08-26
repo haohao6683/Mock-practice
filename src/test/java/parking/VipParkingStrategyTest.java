86e303dea0e6a58ec483f495b301fd109b875ec1
haohao6683 <852721803@qq.com>
2020-08-26 17:15:31 +0800

    [Mocking][Shao & Draymond] test: add testPark_givenCarIsNotVipAndAFullParkingLog_thenGiveNoSpaceReceipt.

--- src/test/java/parking/VipParkingStrategyTest.java
+++ src/test/java/parking/VipParkingStrategyTest.java
@@ -35,7 +35,19 @@ public class VipParkingStrategyTest {
 
         /* Exercise 4, Write a test case on VipParkingStrategy.park()
          * With using Mockito spy, verify and doReturn */
+        //given
+        Car car = mock(Car.class);
+        ParkingLot parkingLot = mock(ParkingLot.class);
+        when(parkingLot.isFull()).thenReturn(true);
+        VipParkingStrategy spyVipParkingStrategy = spy(new VipParkingStrategy());
+        doReturn(false).when(spyVipParkingStrategy).isAllowOverPark(car);
 
+        //when
+        spyVipParkingStrategy.park(Collections.singletonList(parkingLot), car);
+
+        //then
+        verify(spyVipParkingStrategy, times(1)).createNoSpaceReceipt(car);
+        verify(spyVipParkingStrategy, times(0)).createReceipt(parkingLot, car);
     }
 
     @Test
